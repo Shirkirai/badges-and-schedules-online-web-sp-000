b240a72d99e293ddbd140c028f8a6fc00339d985
Jung Lee <lee.jung.s95@gmail.com>
2020-04-18 05:58:53 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,6 +4,6 @@ def badge_maker(name)
 end
 def batch_badge_creator(attendees)
   badge = []
-  attendees.each {|name| badge.shift("Hello, my name is #{name}".)}
+  attendees.each {|name| badge.shift("Hello, my name is #{name}.")}
   end
 end
